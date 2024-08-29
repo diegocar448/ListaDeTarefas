9df0e37b6ae7ec2465f9a3ebd97f11c71c32ceeb
Diego Cardoso <diegocar448@hotmail.com>
2024-08-29 14:09:32 -0300

    arquivos iniciais do projeto

---
+++ app/src/main/java/com/diego/listadetarefascompose/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.diego.listadetarefascompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.diego.listadetarefascompose.ui.theme.ListaDeTarefasComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ListaDeTarefasComposeTheme {
+
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/diego/listadetarefascompose/view/SalvarTarefa.kt
@@ -0,0 +1,8 @@
+package com.diego.listadetarefascompose.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SalvarTarefa(){
+
+}
