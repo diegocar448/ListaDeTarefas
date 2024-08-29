5d7fd0f25607109fd54fe04b552ee4381468f52f
Diego Cardoso <diegocar448@hotmail.com>
2024-08-29 16:40:20 -0300

    Criando Layout Lista Tarefa

--- app/src/main/java/com/diego/listadetarefascompose/view/ListaTarefas.kt
+++ app/src/main/java/com/diego/listadetarefascompose/view/ListaTarefas.kt
@@ -1,12 +1,72 @@
 package com.diego.listadetarefascompose.view
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.diego.listadetarefascompose.R
+import com.diego.listadetarefascompose.ui.theme.BLACK
+import com.diego.listadetarefascompose.ui.theme.Purple700
+import com.diego.listadetarefascompose.ui.theme.WHITE
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ListaTarefas(
     navController: NavController
 ){
-    Text(text = "Lista de Tarefas")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Purple700,
+                    titleContentColor = Color.White,
+                ),
+                title = {
+                    Text(
+                        "Lista de Tarefas",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = WHITE
+                    )
+                }
+            )
+        },
+        containerColor = BLACK,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate(route = "salvarTarefa")
+                },
+                containerColor = Purple700,
+            ) {
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_add),
+                    contentDescription = "Icone de salvar tarefa"
+                )
+            }
+        }
+
+    ) {
+
+    }
 }
